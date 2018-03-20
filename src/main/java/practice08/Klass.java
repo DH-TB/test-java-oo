78ec630ad34814c5840a504792e51203e918e490
Lizhen Huang <lzhuang@CNlzhuang.local>
2018-03-20 11:22:52 +0800

    finish
    Signed-off-by: Lizhen Huang <lzhuang@CNlzhuang.local>

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,38 @@
 package practice08;
 
 public class Klass {
+
+    private Integer number;
+    private Student leader;
+
+    public Klass(Integer number) {
+        this.number = number;
+    }
+    public Klass(){
+
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Klass klass = (Klass) o;
+        return number == klass.number;
+    }
+
+    public String getDisplayName(){
+        return "Class "+this.number;
+    }
+
+    public void assignLeader(Student student){
+        this.leader = student;
+    }
 }
